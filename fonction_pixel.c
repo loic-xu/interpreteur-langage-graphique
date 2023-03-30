1adfaebaf8bbe3f89f9c44b18ff749eea6785412
loic-xu <123300116+loic-xu@users.noreply.github.com>
2023-03-30 20:40:03 +0200

    aaa

--- fonction_pixel.c
+++ fonction_pixel.c
@@ -7,7 +7,7 @@
 // Calcul du pixel courant
 /* 
     requires : seau_opacite et seau_couleur
-    assigns : nothing
+    assigns : nothingaaa
     ensures : retourne le pixel courant
 */
 pixel calcul_pixel_courant(list_opacite seau_opacite, list_couleur seau_couleur) 
